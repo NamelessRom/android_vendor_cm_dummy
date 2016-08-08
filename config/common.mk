91c28cc542af1a651df909a14736a641fe9d0034
Steve Kondik <steve@cyngn.com>
2016-08-07 23:21:29 -0700

    cm: Add custom off-mode charging screen
      * Moving this to open-source from CyanogenOS :)
      * Thanks to Chao Chen, Ethan Chen, Pat Erley, Scott Mertz,
        and Keith Mok for various contributions to this.
    
    Change-Id: If79f9c279668d14ee0cde62889bb09e7185ef08c

--- config/common.mk
+++ config/common.mk
@@ -1,3 +1,12 @@
 # Backup Services whitelist
 PRODUCT_COPY_FILES += \
     vendor/cm/config/permissions/backup.xml:system/etc/sysconfig/backup.xml
+
+# Custom off-mode charger
+ifneq ($(WITH_CM_CHARGER),false)
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    cm_charger_res_images \
+    font_log.png \
+    libhealthd.cm
+endif
